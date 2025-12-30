bb533a2b87e29dd4d7c3169302db16986ad16636
najibaffane <79713782+najibaffane@users.noreply.github.com>
2025-12-30 11:58:08 +0200

    feat: Use Terser for production minification
    Switches the build tool for minification from esbuild to Terser. This provides more robust and potentially smaller minified bundles for production.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -10,7 +11,7 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     sourcemap: false,
-    minify: 'esbuild',
+    minify: 'terser',
     rollupOptions: {
       input: {
         main: './index.html',
